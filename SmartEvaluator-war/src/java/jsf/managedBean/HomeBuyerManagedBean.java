5e447bae93eb17af1353c15ec1765875faa4811d
xuhong98 <>
2018-09-22 12:02:30 +0800

    update

--- SmartEvaluator-war/src/java/jsf/managedBean/HomeBuyerManagedBean.java
+++ SmartEvaluator-war/src/java/jsf/managedBean/HomeBuyerManagedBean.java
@@ -5,6 +5,7 @@
  */
 package jsf.managedBean;
 
+import ejb.session.stateless.AdminControllerLocal;
 import ejb.session.stateless.HomeBuyerControllerLocal;
 import entity.HomeBuyer;
 import entity.House;
@@ -17,6 +18,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.bean.ManagedBean;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import uitl.exceptions.InvalidLoginCredentialException;
 
 /**
  *
@@ -27,12 +29,21 @@ import javax.faces.application.FacesMessage;
 @SessionScoped
 public class HomeBuyerManagedBean implements Serializable{
 
+    @EJB(name = "AdminControllerLocal")
+    private AdminControllerLocal adminControllerLocal;
+
     @EJB(name = "HomeBuyerControllerLocal")
     private HomeBuyerControllerLocal homeBuyerControllerLocal;
 
     private HomeBuyer homeBuyer;
     
     private List<House> houseList;
+    
+    private String adminUsername;
+    
+    private String adminPassword;
+    
+    private String married;
     
     public HomeBuyerManagedBean() {
         homeBuyer = new HomeBuyer();
@@ -53,8 +64,49 @@ public class HomeBuyerManagedBean implements Serializable{
     public void setHomeBuyer(HomeBuyer homeBuyer) {
         this.homeBuyer = homeBuyer;
     }
+
+    public String getMarried() {
+        return married;
+    }
+
+    public void setMarried(String married) {
+        this.married = married;
+    }
+
+    public String getAdminUsername() {
+        return adminUsername;
+    }
+
+    public void setAdminUsername(String adminUsername) {
+        this.adminUsername = adminUsername;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+    
+    public void adminLogin(ActionEvent event) throws IOException{
+        try {
+            Boolean flag = adminControllerLocal.login(adminUsername, adminPassword);
+            FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isLogin", true);
+            FacesContext.getCurrentInstance().getExternalContext().redirect("adminPage.xhtml");
+        } catch (InvalidLoginCredentialException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid login credential: " + ex.getMessage(), null));
+        }
+    }
     
     public void exploreHouse(ActionEvent event) throws IOException {
+        if(married.equals("true")){
+            homeBuyer.setMarried(true);
+        }else{
+            homeBuyer.setMarried(false);
+        }
+        
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("name", homeBuyer.getName());
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("age", homeBuyer.getAge());
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("savings", homeBuyer.getSavings());
